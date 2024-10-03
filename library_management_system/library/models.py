f4e96ee687156a0d2a200ef736a9b6ebc4775f3b
Ashfaq3442 <ashfaqm46@gmail.com>
2024-10-04 03:21:16 +0500

    Library Managment system

---
+++ library_management_system/library/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models
+
+# Create your models here.
